f93f1858f52c2ba923730fb46952cc369ba2b80b
Snorre Ledal <snorreledal@hotmail.no>
2023-11-14 15:24:10 +0100

    Configured the sam app to work through CLI

--- kjell/hello_world/app.py
+++ kjell/hello_world/app.py
@@ -16,7 +16,7 @@ s3_client = boto3.client('s3', region_name='eu-west-1')
 rekognition_client = boto3.client('rekognition', region_name='eu-west-1')
 
 # Oppgave 1A
-BUCKET_NAME = "kjellsimagebucker"
+BUCKET_NAME = "candidate2014"
 
 def lambda_handler(event, context):
 
