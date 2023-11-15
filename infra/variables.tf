a30c18d0388f07e976f35e7b8bcb14c9387e5948
Snorre Ledal <snorreledal@hotmail.no>
2023-11-15 17:48:21 +0100

    implemented terraform

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,3 +6,17 @@ variable "region" {
   type = string
   default = "eu-west-1"
 }
+
+variable "apprunner_image" {
+  type = string
+}
+
+variable "apprunner_cpu" {
+  type = string
+  default = "256"
+}
+
+variable "apprunner_memory" {
+  type = string
+  default = "1024"
+}
